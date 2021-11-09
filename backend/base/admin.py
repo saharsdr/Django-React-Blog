ba17f1e809f7e6955ad9ac73c9bafa86e8118b61
Sahar Sadri <sahar.sadrii.ss@gmail.com>
2021-11-09 11:12:29 +0330

    2- Make the migration and models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import PostCategory, Category, Comment, Follow, Like, Post
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(PostCategory)
+admin.site.register(Follow)
+admin.site.register(Like)
+admin.site.register(Comment)
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=True)
+    author = models.CharField(max_length=200, null=False, blank=True)
+    content = models.TextField(null=True, blank=True)
+    # thumbnail_pic=
+    descriprion = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class PostCategory(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Like(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.createdAt)
+
+
+class Follow(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following")
